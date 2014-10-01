42c200fe10cb275327a0aac94c5a45cba5ed529d
Elise <koster.elise@gmail.com>
2014-10-01 17:00:14 +0200

    Changed bullets for pro/con list to +/- signs

--- django_codecult/admin.py
+++ django_codecult/admin.py
@@ -4,9 +4,6 @@ from django_summernote.admin import SummernoteModelAdmin
 
 class AdminBlock(SummernoteModelAdmin):
 	base_model = Block
-
-class AdminListblock(SummernoteModelAdmin):
-	base_model = Listblock
  
 class AdminPage(SummernoteModelAdmin):
 	base_model = Page
@@ -15,7 +12,7 @@ class AdminIndex(SummernoteModelAdmin):
 	base_model = Index
 
 admin.site.register(Block, AdminBlock)
-admin.site.register(Listblock, AdminListblock)
+admin.site.register(Listblock)
 admin.site.register(Page, AdminPage)
 admin.site.register(Index, AdminIndex)
 admin.site.register(Imageslider)
