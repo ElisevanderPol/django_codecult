139b8ca2ef790b5f81d0b58bd47f113fcd6e17be
Elise <koster.elise@gmail.com>
2014-10-01 16:45:47 +0200

    Image slider basic version done

--- django_codecult/static/js/custom.js
+++ django_codecult/static/js/custom.js
@@ -1,3 +1,24 @@
 function textToList(text) {
 	return text.split(',');
 }
+
+$(".slider-button").click(function(){
+	// Get source of image corresponding to selected button
+	var src = $(this).find('img').attr('src');
+	// Get id of clicked button
+	var id = $(this).attr('id');
+	// Change the source of the displayed image
+	$('#image-visible').find('img').attr('src', src);
+	// Get the class corresponding to the previous button id
+	var old_id = $('#image-visible').attr('class');
+	// Change shown image class to the new id
+	$('#image-visible').attr('class', id);
+	// Change the old button to the 'not-selected' class
+	$('#'+old_id).attr('class', 'not-selected slider-button');
+	// Add class 'selected-button' to this button
+	$(this).attr('class', 'selected-button slider-button');
+})
+
+$(document).ready(function(){
+	$('#0').attr('class', 'selected-button slider-button');
+})
