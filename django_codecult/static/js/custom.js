ed719d0cba4afbfec04c2a85bb8986074a32b635
Elise <koster.elise@gmail.com>
2014-11-06 13:44:59 +0100

    Fixed multiple imagesliders; now work as intended

--- django_codecult/static/js/custom.js
+++ django_codecult/static/js/custom.js
@@ -7,12 +7,14 @@ $(".slider-button").click(function(){
 	var src = $(this).find('img').attr('src');
 	// Get id of clicked button
 	var id = $(this).attr('id');
+	// Get primary key of image slider object
+	var pk = id.split('-')[1];
 	// Change the source of the displayed image
-	$('#image-visible').find('img').attr('src', src);
+	$('#image-visible-'.concat(pk)).find('img').attr('src', src);
 	// Get the class corresponding to the previous button id
-	var old_id = $('#image-visible').attr('class');
+	var old_id = $('#image-visible-'.concat(pk)).attr('class');
 	// Change shown image class to the new id
-	$('#image-visible').attr('class', id);
+	$('#image-visible-'.concat(pk)).attr('class', id);
 	// Change the old button to the 'not-selected' class
 	$('#'+old_id).attr('class', 'not-selected slider-button');
 	// Add class 'selected-button' to this button
